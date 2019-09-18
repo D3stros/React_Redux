76f6dbc511b63955647e2f365ac119d308a59e0c
Destros <patrick.fallmann@hotmail.com>
2019-09-18 22:21:17 +0200

    Implemented onclick for button with dispatch increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions/index";
 
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter{counter}</h1>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? (
         <h3>Valuable Information that should not be visible without Login</h3>
       ) : (
